89ad896f5aca36b3f745f074fa5235b134fabfdf
vanbsanne <sannevanbrabant@hotmail.com>
2025-05-29 19:12:42 +0200

    added function to select categories

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-console.log("HELLO");
+// Set Date in header
+let dateTitle = document.getElementById('date');
+const date = new Date();
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = date.toLocaleDateString('en-US', options);
+dateTitle.innerText = formattedDate;
+
+let group1, group2, group3, group4;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function selectCategories() {
+    let max = categories.length;
+    let randomNumbers = new Set([]);
+
+    while (randomNumbers.size < 4) {
+        let random = getRandomInt(max);
+        randomNumbers.add(random);
+    }
+
+    console.log("set: ", randomNumbers)
+}
+
+selectCategories();
---
+++ words.js
@@ -0,0 +1,74 @@
+const categories = [
+    {
+        group: "Vehicles",
+        words: [
+            "Car",
+            "Truck",
+            "Van",
+            "Scooter"
+        ]
+    },
+    {
+        group: "Colors",
+        words: [
+            "Red",
+            "Blue",
+            "Green",
+            "Yellow"
+        ]
+    },
+    {
+        group: "Sodas",
+        words: [
+            "Coke",
+            "Fanta",
+            "Sprite",
+            "Iced Tea"
+        ]
+    },
+    {
+        group: "Monsters",
+        words: [
+            "Vampire",
+            "Mummy",
+            "Werewolf",
+            "Zombie"
+        ]
+    },
+    {
+        group: "Sodas1",
+        words: [
+            "Coke",
+            "Fanta",
+            "Sprite",
+            "Iced Tea"
+        ]
+    },
+    {
+        group: "Sodas2",
+        words: [
+            "Coke",
+            "Fanta",
+            "Sprite",
+            "Iced Tea"
+        ]
+    },
+    {
+        group: "Sodas3",
+        words: [
+            "Coke",
+            "Fanta",
+            "Sprite",
+            "Iced Tea"
+        ]
+    },
+    {
+        group: "Sodas4",
+        words: [
+            "Coke",
+            "Fanta",
+            "Sprite",
+            "Iced Tea"
+        ]
+    }
+]
